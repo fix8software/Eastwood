420882120a00a98c2ca3556aaeb09ebcb3201a6c
naphthasl <54272789+naphthasl@users.noreply.github.com>
2019-08-25 19:53:07 +0100

    Significantly simplified Plasma.

--- eastwood/plasma.py
+++ eastwood/plasma.py
@@ -5,29 +5,12 @@ from psutil import cpu_count
 from multiprocessing.pool import ThreadPool
 
 # Import different algorithms for various stages of compression
-import bz2 as algo # Used for all blocks
-import lzma as backup # Used for blocks with dangerous sizes
-import zstd as single # Used for finalizing output
+import zstd as algo # Used for all blocks
 
 # These variables are the ones that probably won't break anything if you change them.
 # Please note that these values must be the same for both the compressor and decompressor.
 SIZE_BYTES = 4
-BITFLAG_BYTES = 1
-LEVEL_BYTES = 4
-BACKUP_ENABLED = True # Specifies if LZMA should be used when Bzip2 fails
-BACKUP_PRESET = 4
-
-# These are pretty important in general and you shouldn't touch them at all.
-BACKUP_FILTERS = [
-	{"id": backup.FILTER_LZMA1, "preset": BACKUP_PRESET}
-]
 BYTE_ORDER = 'little'
-FINALIZED_BITFLAGS = {
-	'finalized': int(0b00000001).to_bytes(BITFLAG_BYTES, byteorder=BYTE_ORDER),
-	'unfinalized': int(0b00000000).to_bytes(BITFLAG_BYTES, byteorder=BYTE_ORDER),
-	'notcompressed': int(0b00000010).to_bytes(BITFLAG_BYTES, byteorder=BYTE_ORDER),
-	'full-lzma': int(0b00000011).to_bytes(BITFLAG_BYTES, byteorder=BYTE_ORDER)
-}
 
 class ParallelCompressionInterface(object):
 	"""
@@ -42,11 +25,11 @@ class ParallelCompressionInterface(object):
 		self.__pool = ThreadPool(nodes)
 		self.__internal_node_count = nodes
 
-	def __level_arguments(self, chunk: bytes, level: int, full_lzma: bool) -> tuple:
+	def __level_arguments(self, chunk: bytes, level: int) -> tuple:
 		"""
 		Private function to automatically prepare arguments for internal compression and starmapping.
 		"""
-		return (chunk, level, full_lzma)
+		return (chunk, level)
 
 	def __chunks(self, l, n):
 		"""
@@ -55,7 +38,7 @@ class ParallelCompressionInterface(object):
 		for i in range(0, len(l), n):
 			yield l[i:i+n]
 
-	def compress(self, input: bytes, level: int = 9, chunks: int = -1, finalize: bool = True, finalize_preset: int = 3, finalize_threshold: int = 2048, lzma_threshold = -1) -> bytes:
+	def compress(self, input: bytes, level: int = 9, chunks: int = -1) -> bytes:
 		"""
 		Main compression function.
 		Args:
@@ -63,38 +46,14 @@ class ParallelCompressionInterface(object):
 			level: Compression level
 			chunks: Chunks to compress with
 		"""
-		if level < 1:
-			return FINALIZED_BITFLAGS['notcompressed'] + input
 
 		if chunks < 1:
 			chunks = self.__internal_node_count
 
-		if len(input) > lzma_threshold and lzma_threshold > 1:
-			full_lzma = True
-		else:
-			full_lzma = False
-
 		chunks = list(self.__chunks(input, (lambda x: x if x != 0 else 1)(int(round(len(input) / chunks)))))
-		chunks = self.__pool.imap(_internal_compression, [self.__level_arguments(c, level, full_lzma) for c in chunks])
+		chunks = self.__pool.imap(_internal_compression, [self.__level_arguments(c, level) for c in chunks])
 
-		raw = b''.join(chunks)
-		
-		if full_lzma == True:
-			rv = FINALIZED_BITFLAGS['full-lzma'] + raw
-		else:
-			if finalize == True and len(raw) >= finalize_threshold:
-				finalized = single.compress(raw, finalize_preset)
-				if len(finalized) >= len(raw):
-					rv = FINALIZED_BITFLAGS['unfinalized'] + raw
-
-				rv = FINALIZED_BITFLAGS['finalized'] + finalized
-			else:
-				rv = FINALIZED_BITFLAGS['unfinalized'] + raw
-
-		if len(rv) > len(input):
-			return FINALIZED_BITFLAGS['notcompressed'] + input
-		else:
-			return rv
+		return b''.join(chunks)
 
 	def decompress(self, input: bytes) -> bytes:
 		"""
@@ -102,12 +61,6 @@ class ParallelCompressionInterface(object):
 		Args:
 			input: Bytes to decompress - Note this is not compatible with the output of the standard compression function.
 		"""
-		if   input[:BITFLAG_BYTES] == FINALIZED_BITFLAGS['finalized']:
-			input = single.decompress(input[BITFLAG_BYTES:])
-		elif input[:BITFLAG_BYTES] == FINALIZED_BITFLAGS['notcompressed']:
-			return input[BITFLAG_BYTES:]
-		else:
-			input = input[BITFLAG_BYTES:]
 
 		chunks = []
 		while len(input) > 0:
@@ -126,49 +79,7 @@ def _decompress(input: bytes) -> bytes:
 def _internal_compression(args) -> bytes:
 	capsule = _compress(args[0], args[1])
 
-	info_bitflag = 0b00000000
-
-	if args[2] == True:
-		FILTERS = [{"id": backup.FILTER_LZMA2, "preset": args[1]}]
-		capsule = args[1].to_bytes(LEVEL_BYTES, byteorder=BYTE_ORDER) + backup.compress(args[0], format=backup.FORMAT_RAW, filters=FILTERS)
-		info_bitflag = 0b00000011
-	else:	
-		if  len(capsule) < len(args[0]):
-			info_bitflag = 0b00000001
-		elif BACKUP_ENABLED == True:
-			capsule = backup.compress(args[0], format=backup.FORMAT_RAW, filters=BACKUP_FILTERS)
-			if   len(capsule) < len(args[0]):
-				info_bitflag = 0b00000010
-			else:
-				capsule = args[0]
-		else:
-			capsule = args[0]
-
-	return (len(capsule)+1).to_bytes(SIZE_BYTES, byteorder=BYTE_ORDER) + capsule + int(info_bitflag).to_bytes(BITFLAG_BYTES, byteorder=BYTE_ORDER)
+	return len(capsule).to_bytes(SIZE_BYTES, byteorder=BYTE_ORDER) + capsule
 
 def _internal_decompression(input: bytes) -> bytes:
-	BITFLAG = int.from_bytes(input[-BITFLAG_BYTES:], byteorder=BYTE_ORDER)
-	if   BITFLAG == 0b00000001:
-		finished = _decompress(input[:-BITFLAG_BYTES])
-	elif BITFLAG == 0b00000010:
-		finished = backup.decompress(input, format=backup.FORMAT_RAW, filters=BACKUP_FILTERS)
-	elif BITFLAG == 0b00000011:
-		FILTERS = [{"id": backup.FILTER_LZMA2, "preset": int.from_bytes(input[:-BITFLAG_BYTES][:LEVEL_BYTES], byteorder=BYTE_ORDER)}]
-		finished = backup.compress(input[:-BITFLAG_BYTES][LEVEL_BYTES:], format=backup.FORMAT_RAW, filters=FILTERS)
-	elif BITFLAG == 0b00000000:
-		finished = input[:-BITFLAG_BYTES]
-
-	return finished
-	
-if __name__ == '__main__':
-	import os, time
-	x = ParallelCompressionInterface()
-	#data = (b'\x00' * (1024*1024))
-	data = os.urandom(1024*1024)
-	et = time.time()
-	a = x.compress(data)
-	print((time.time() - et) * 1000)
-	et = time.time()
-	b = x.decompress(a)
-	print((time.time() - et) * 1000)
-	assert b == data
+	return _decompress(input)
