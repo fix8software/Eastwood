4276d36bd6fd60debf6f959627bca2d591f0be78
Ethan Wong <epicface92@gmail.com>
2019-08-27 19:10:41 -0500

    Order packets by type when sending poems

--- eastwood/ew_packet.py
+++ eastwood/ew_packet.py
@@ -13,6 +13,7 @@ packet_names = [
 	# packet id # 0
 	# fields:
 	# 	per packet:
+	#		int: the order/index of the packet
 	#		uuid: the user/sender of the packet
 	#		byte array: the packet itself
 	("delete_conn", "upstream"),
